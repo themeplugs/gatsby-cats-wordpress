2f37f2ad43de6d514c6e57e271d77bb930354237
xpeedshagor <shagor@themeplugs.com>
2021-05-08 02:04:07 +0600

    revert all code post list

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,50 +5,43 @@ import { Link, graphql } from 'gatsby'
 export default class IndexPage extends React.Component {
   render() {
     const { posts, title } = this.props
-    const resolutions = post.featured_media.localFile.childImage.Sharp.resolutions
+
     return (
       <section className="section x">
         <div className="container">
-          <div className="columns">
-              <div className="column">
-                <div className="columns">
-                    {posts.map(({ node: post }) => (
-                      <div className="post-content column is-6" key={post.id} >
-                       {resolutions &&
-                          <div>
-                            <img src="{resolutions.src}" alt="" />
-                          </div>
-                       }
-                        <p>
-                          <Link className="has-text-primary" to={post.slug}>
-                            {post.title}
-                          </Link>
-                          <span> &bull; </span>
-                          <small>
-                            {post.date} - posted by{' '}
-                            <Link to={`/author/${post.author.slug}`}>
-                              {post.author.name}
-                            </Link>
-                          </small>
-                        </p>
-                        <div>
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                            }}
-                          />
-                          <Link className="button is-small" to={post.slug}>
-                            Keep Reading →
-                          </Link>
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
-              <div className="column is-one-third">
-
-              </div>
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">{title}</h1>
           </div>
+          {posts.map(({ node: post }) => (
+            <div
+              className="content"
+              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+              key={post.id}
+            >
+              <p>
+                <Link className="has-text-primary" to={post.slug}>
+                  {post.title}
+                </Link>
+                <span> &bull; </span>
+                <small>
+                  {post.date} - posted by{' '}
+                  <Link to={`/author/${post.author.slug}`}>
+                    {post.author.name}
+                  </Link>
+                </small>
+              </p>
+              <div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: post.excerpt.replace(/<p class="link-more.*/, ''),
+                  }}
+                />
+                <Link className="button is-small" to={post.slug}>
+                  Keep Reading →
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     )
