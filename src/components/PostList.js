be43ea1462d4ee2a405ba466000dba6923231d3a
xpeedshagor <shagor@themeplugs.com>
2021-05-08 18:04:51 +0600

    grid update

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,11 +9,11 @@ export default class IndexPage extends React.Component {
     return (
       <section className="section x">
         <div className="container">
-          <div className="columns">
-              <div className="column">
-                <div className="columns">
+          <div className="row">
+              <div className="col-lg-8">
+                <div className="row">
                     {posts.map(({ node: post }) => (
-                      <div className="post-content column is-6" key={post.id} >
+                      <div className="col-lg-6" key={post.id} >
                         <p>
                           <Link className="has-text-primary" to={post.slug}>
                             {post.title}
@@ -40,40 +40,10 @@ export default class IndexPage extends React.Component {
                     ))}
                 </div>
               </div>
-              <div className="column is-one-third">
+              <div className="col-lg-4">
 
               </div>
           </div>
-          {posts.map(({ node: post }) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
-              <p>
-                <Link className="has-text-primary" to={post.slug}>
-                  {post.title}
-                </Link>
-                <span> &bull; </span>
-                <small>
-                  {post.date} - posted by{' '}
-                  <Link to={`/author/${post.author.slug}`}>
-                    {post.author.name}
-                  </Link>
-                </small>
-              </p>
-              <div>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                  }}
-                />
-                <Link className="button is-small" to={post.slug}>
-                  Keep Reading →
-                </Link>
-              </div>
-            </div>
-          ))}
         </div>
       </section>
     )
