69f063106ac9d7be85944ec5e9fddba33e780aa3
xpeedshagor <shagor@themeplugs.com>
2021-05-09 00:45:52 +0600

    update menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,36 +2,14 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from './Navbar'
-
-import NavMenu from './NavMenu'
-
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + WordPress" />
-    <NavMenu menu={data} />
+    <Navbar />
     <div>{children}</div>
   </div>
 )
 
 export default TemplateWrapper
-
-
-export const query = graphql`
-  query LayoutQuery {
-        allWordpressWpApiMenusMenusItems{
-            edges{
-                node{
-                    id
-                    name
-                    items{
-                        title
-                        url
-                        object_slug
-                    }
-                }
-            }
-        }
-  }
-`
